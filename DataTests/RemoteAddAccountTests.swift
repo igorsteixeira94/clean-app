1f1a292bc4f1e3404e419f13a7ca63daa8f3869e
Igor Rodrigues <igorsteixeira94@gmail.com>
2023-01-03 08:49:47 -0300

    add RemoteAddAcountTest add method

---
+++ DataTests/RemoteAddAccountTests.swift
@@ -0,0 +1,37 @@
+import XCTest
+
+class RemoteAddAccount {
+    private let url: URL
+    private let httpClient: HttpPostClient
+    
+    init(url: URL, httpClient: HttpPostClient) {
+        self.url = url
+        self.httpClient = httpClient
+    }
+    
+    func add() {
+        httpClient.post(url: url)
+    }
+}
+
+protocol HttpPostClient {
+    func post(url: URL)
+}
+
+final class RemoteAddAccountTests: XCTestCase {
+    func test_add_should_call_httpClient_with_correct_url() {
+        let url = URL(string: "http://any-url.com")!
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        sut.add()
+        XCTAssertEqual(httpClientSpy.url, url)
+    }
+    
+    class HttpClientSpy: HttpPostClient {
+        var url: URL?
+        
+        func post(url: URL) {
+            self.url = url
+        }
+    }
+}
