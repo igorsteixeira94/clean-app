7da966a08e127aa30f555a617481a559da6ca2bc
Igor Rodrigues <igorsteixeira94@gmail.com>
2023-01-12 21:30:50 -0300

    Test call Alamofire with data

--- DataTests/Helpers/TestFactories.swift
+++ DataTests/Helpers/TestFactories.swift
@@ -4,6 +4,10 @@ func makeInvalidData() -> Data {
     return Data("invlaid_data".utf8)
 }
 
+func makeValidData() -> Data {
+    return Data("{\"name\": \"Igor\"}".utf8)
+}
+
 func makeURL() -> URL {
     URL(string: "http://any-url.com")!
 }
